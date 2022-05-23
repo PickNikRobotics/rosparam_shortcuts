062c567d1524023a7fea009ae1bdf1897849e359
Lucas Walter <wsacul@gmail.com>
2022-05-22 17:10:47 -0700

    remove space at end of line so roslint passes

--- src/rosparam_shortcuts.cpp
+++ src/rosparam_shortcuts.cpp
@@ -284,7 +284,7 @@ bool convertDoublesToEigen(const std::string &parent_name, std::vector<double> v
   else if (values.size() == 7)
   {
     // Quaternion
-    transform = Eigen::Translation3d(values[0], values[1], values[2]) * 
+    transform = Eigen::Translation3d(values[0], values[1], values[2]) *
                 Eigen::Quaterniond(values[3], values[4], values[5], values[6]);
     return true;
   }
